a28761617a865f22cb1a08be0f49611c0536c133
pablo2389 <160980965+pablo2389@users.noreply.github.com>
2024-06-12 21:34:18 -0300

    Add files via upload

--- ejercicios_practica/1- storage/js/script.js
+++ ejercicios_practica/1- storage/js/script.js
@@ -21,6 +21,35 @@ con los datos almacenados en memoria, a fin de darle la bienvenida al usuario lo
 
 */
 
+/*   let boton = document.getElementById("btningresar");
+boton.addEventListener("click", () => {
+    let usuario = document.getElementById("usuario").value; 
+    localStorage.setItem("uno", usuario);
+    document.getElementById("usuario").value = ""; 
+    let email = document.getElementById("email").value; 
+    localStorage.setItem("dos", email);
+    document.getElementById("email").value = ""; 
+});   */
+const usuarioInput = document.getElementById("usuario");
+const emailInput = document.getElementById("email");
+const ingresarButton = document.getElementById("btnIngresar");
+
+
+ingresarButton.addEventListener("click", () => {
+    const usuario = usuarioInput.value;
+    const email = emailInput.value;
+
+    
+    localStorage.setItem("usuario", usuario);
+    localStorage.setItem("email", email);
+
+
+    usuarioLogeado.textContent = `Bienvenido, ${usuario}!`;
+    emailLogeado.textContent = `Email: ${email}`;
+
+  
+});
+
 /* 2 - Enunciado
 
 - Al comenzar el script se debe verificar si en memoria se encuentran
@@ -29,8 +58,38 @@ almacenados los datos de "usuario" y "email".
 "usuarioLogeado" y "emailLogeado".
 - Se debe ocultar la sección "login" (puede usar la clase CSS hidden ya creada)
 y deberá revelar la sección "logout" (quitar la clase hidden).
-
 */
+/* const botoningresar = document.getElementById("btnIngresar");
+const usuario = document.getElementById("usuario");
+const email = document.getElementById("email");
+const usuarioLogeado = document.getElementById("usuariologeado");
+const emailLogeado = document.getElementById("emailLogeado");
+
+const usuarioAlmacenado = localStorage.getItem("usuario");
+const emaiLAlmacenado = localStorage.getItem("email");
+if(usuarioAlmacenado&& emaiLAlmacenado){
+    usuarioLogeado.textContent = `¡bienvenido, ${usuarioAlmacenado}!`
+    emailLogeado.textContent = `Email: ${emaiLAlmacenado}`;
+
+    seccionLogin.classList.add("hidden");
+    seccionLogout.classList.remove("hidden");
+    botonIngresar.addEventListener("click", function() {
+        
+        const usuario = inputUsuario.value;
+        const email = inputEmail.value;
+
+        localStorage.setItem("usuario", usuario);
+        localStorage.setItem("email", email);
+    
+  
+        usuarioLogeado.textContent = `¡Bienvenido, ${usuario}!`;
+        emailLogeado.textContent = `Email: ${email}`;
+    
+        seccionLogin.classList.add("hidden");
+        seccionLogout.classList.remove("hidden");
+    });
+
+}  
 
 /* 3 - Enunciado
 
@@ -41,3 +100,14 @@ volverse a cargar debería aparecer nuevamente la sección de bienvenida
 ya que no debería haber más datos en memoria cargados.
 
 */
+const botonSalir = document.getElementById("btnSalir");
+
+
+botonSalir.addEventListener("click", function() {
+
+    localStorage.removeItem("usuario");
+    localStorage.removeItem("email");
+
+
+    window.location.reload();
+});
